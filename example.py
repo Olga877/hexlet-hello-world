03ae8f4f123b3ad08f69dea77de0cef770cf1159
Olga <o.gavrikova877@gmail.com>
2024-12-12 11:41:45 +0400

    add new files

--- example.py
+++ example.py
@@ -1,1 +1,6 @@
 print("Hello from python project!")
+from more_itertools import flatten, substrings
+
+coll = [(0, 1), (2, 3)]
+# делаем коллекцию плоской
+print(list(flatten(coll))) # => [0, 1, 2, 3]
